de25c9d39db8921a85279a27665bf6c042d3a2a5
luoshiqi <luoshiqi@bytedance.com>
2026-06-11 20:15:41 +0800

    Enable web loading of legacy Android states

--- pkg/emscripten/libretro/libretro.js
+++ pkg/emscripten/libretro/libretro.js
@@ -5,7 +5,7 @@
  */
 
 const defaultCore = "dosbox_pure";
-const coreAssetVersion = "20260611-194950";
+const coreAssetVersion = "20260611-201520";
 const frontendAssetVersion = coreAssetVersion;
 const authApiBase = "/api/sync/v1/auth";
 var autoStart = true;
